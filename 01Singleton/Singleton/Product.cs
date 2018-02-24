6358396fb76089a52279b389510e5b3d9a2ccd20
Artur Koliński <secorek@gmail.com>
2018-02-24 16:03:15 +0100

    singleton example with thread safety

---
+++ 01Singleton/Singleton/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton
+{
+    public class Product
+    {
+        public string Name;
+        public int Price;
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ 01Singleton/Singleton/ShoppingList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton
+{
+    public class ShoppingList
+    {
+        private static ShoppingList _shoppingListInstance;
+        private readonly List<Product> _productList;
+        private static readonly object MyLock = new object();
+        public static ShoppingList ShoppingListInstance
+        {
+            get
+            {
+                if (_shoppingListInstance == null)
+                {
+                    lock (MyLock)
+                    {
+                        if (_shoppingListInstance == null)
+                        {
+                            _shoppingListInstance = new ShoppingList();
+                        }
+
+                    }
+                }
+                return _shoppingListInstance;
+            }
+        }
+        private ShoppingList()
+        {
+            _productList = new List<Product>();
+        }
+        public List<Product> GetProductList()
+        {
+            lock (MyLock)
+            {
+                return _productList;
+            }
+        }
+        public void AddProduct(Product product)
+        {
+            lock (MyLock)
+            {
+                _productList.Add(product);
+            }
+        }
+        public void DeleteInstance()
+        {
+                _shoppingListInstance = null;
+        }
+    }
+}
