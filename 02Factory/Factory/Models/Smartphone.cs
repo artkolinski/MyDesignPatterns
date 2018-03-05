41786d775a29bd55103d1a8ebfcb81e3e2997b15
Artur Koliński <secorek@gmail.com>
2018-03-05 23:01:47 +0100

    simple, reflection and abstract factories with tests

---
+++ 02Factory/Factory/Models/Smartphone.cs
@@ -0,0 +1,9 @@
+﻿namespace Factory.Models
+{
+    public class Smartphone : Device
+    {
+        public override string Model => "S8";
+        public override string Manufacturer => "Samsung";
+        public override int Memory => 32;
+    }
+}
