599cb353f8d2e983147c764fec6a55c4a32fbe70
Artur Koliński <secorek@gmail.com>
2018-04-25 23:03:06 +0200

    added prototype pattern with simple reading from combobox

--- 2018_Kolinski_Artur/ComputerProjects/MainWindow.xaml.cs
+++ 2018_Kolinski_Artur/ComputerProjects/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ComputerProjects.Models;
 
 namespace ComputerProjects
 {
@@ -20,14 +12,28 @@ namespace ComputerProjects
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Computer> Computers = new List<Computer>();
+        public List<Motherboard> Motherboards = new List<Motherboard>();
+        
+        public string SelectedProcessor { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            #region declaringObjects
+                
+            #endregion
         }
 
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
+            var comboCaseText = comboCase.Text;
+            var comboMotherboardText = comboMotherboards.Text;
+            var comboProcessorText = comboProcessor.Text;
+            var processor = new Processor(){ Model = comboProcessorText };
+            var motherboard = new Motherboard(){Name = comboMotherboardText,Processor = processor};
+            var computer = new Computer(){Case = comboCaseText, Motherboard = motherboard};
 
+            MessageBox.Show("wartosc to " + comboCaseText + "," +comboMotherboardText+","+comboProcessorText);
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -49,6 +55,55 @@ namespace ComputerProjects
         private void deleteSelectedBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void comboProcessor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void comboProcessor_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> processors = new List<string>();
+            processors.Add("Intel i3" );
+            processors.Add("Intel i5" );
+            processors.Add("Intel i7");
+            var combo = sender as ComboBox;
+            combo.ItemsSource = processors;
+            combo.SelectedIndex = 0;
+        }
+
+        private void comboMotherboards_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> motherboards = new List<string>();
+            motherboards.Add("Asus 432");
+            motherboards.Add("Asus X");
+            motherboards.Add("MSI 22");
+            var combo = sender as ComboBox;
+            combo.ItemsSource = motherboards;
+            combo.SelectedIndex = 0;
+        }
+
+        private void comboCase_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> cases = new List<string>();
+            cases.Add("Small case");
+            cases.Add("Medium case");
+            cases.Add("Big case");
+            var combo = sender as ComboBox;
+            combo.ItemsSource = cases;
+            combo.SelectedIndex = 0;
+        }
+
+        private void comboBuilder_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> builds = new List<string>();
+            builds.Add("First build");
+            builds.Add("Second");
+            builds.Add("Third");
+            var combo = sender as ComboBox;
+            combo.ItemsSource = builds;
+            combo.SelectedIndex = 0;
         }
     }
 }
