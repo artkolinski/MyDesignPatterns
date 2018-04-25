d0ffad131b94466d260972765598b034ddd413e2
Artur Koliński <secorek@gmail.com>
2018-04-25 18:34:23 +0200

    added project with skeleton of user interface

---
+++ 2018_Kolinski_Artur/ComputerProjects/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComputerProjects
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+        private void downloadJSONBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void downloadXMLBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteSelectedBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
